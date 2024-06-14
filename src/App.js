f4aa164f4b55e26cc5c55bf703024b6af109ccdf
camilo <camiloramos99@outlook.com.ar>
2024-06-13 22:56:11 -0300

    Localstorage added function saveItems

--- src/App.js
+++ src/App.js
@@ -4,18 +4,44 @@ import { AddTask } from './AddTask';
 import { TodoList } from './TodoList';
 import { TodoItem } from './TodoItem';
 
-const defaultTodos = [
-  {  text: "Programar", completed: true },
-  {  text: "Hacer el almuerzo", completed: false },
-  {  text: "estudiar inglés", completed: false },
-  {  text: "Tender la cama", completed: false },
-  {  text: "llamar a mamá", completed: false },
-];
+// const defaultTodos = [
+//   {  text: "Programar", completed: true },
+//   {  text: "Hacer el almuerzo", completed: false },
+//   {  text: "estudiar inglés", completed: false },
+//   {  text: "Tender la cama", completed: false },
+//   {  text: "llamar a mamá", completed: false },
+// ];
+// localStorage.setItem('TODOS_V1', JSON.stringify(defaultTodos));
 
+//   localStorage.setItem('TODOS_V1', defaultTodos);
+// Custom Hook para guardar todos (items) de usuario
+
+function useLocalStorage(itemName, initialValue)  {
+  const localStorageItem = localStorage.getItem('itemName') // Trae cada item de localStorage y lo guarda en la variable
+
+  let parsedItem;
+
+  if (!localStorageItem) {                                        //Logica(si no hay item en LS => )
+    localStorage.setItem(itemName, JSON.stringify(initialValue)); //setItem con initialValue (stringified) para localstorage
+    parsedItem = initialValue;                        //Declara(parsedItem) con initial value parseado
+  } else {
+    parsedItem = JSON.parse(localStorageItem);
+  }
+
+  const [item, setItem] = React.useState(parsedItem);    //Estado inicial para uso interno del Custom Hook
+
+
+  const saveItem = (newItem) => {
+    localStorage.setItem(itemName, JSON.stringify(newItem)); //Pasamos a local storage el nuevo item
+    setItem(newItem);
+  };
+
+  return [item, saveItem];
+}
 
 function App() {
 
-  const [todos, setTodos] = React.useState(defaultTodos);
+  const [todos, saveTodos] = useLocalStorage('TODOS_V1', []);       
 
   const completedTodos = todos.filter(todo => todo.completed).length;
 
@@ -28,7 +54,7 @@ function App() {
       (todo) => todo.text === text
       );
     newTodos[todoIndex].completed = true;
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   const deleteTodo = (text) => {
@@ -37,7 +63,7 @@ function App() {
       (todo) => todo.text === text
       );
     newTodos.splice(todoIndex, 1);
-    setTodos(newTodos);
+    saveTodos(newTodos);
   };
 
   
